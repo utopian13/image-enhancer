0782951b8a4694c690e00b4c945a48482084496e
Saad Zia <119245571+utopian13@users.noreply.github.com>
2024-05-05 20:31:04 +0500

    add firebase storage

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import os
-import subprocess
-import time
 from flask import Flask, render_template, request, send_file, redirect, url_for
 from werkzeug.utils import secure_filename
+import firebase_admin
+from firebase_admin import credentials, storage
 
-app = Flask(__name__, template_folder='templates',
-            static_folder='static')
-app.config['UPLOAD_FOLDER'] = os.path.join(
-    os.getcwd(), "static", "uploads")
-app.config['UPSCALED_FOLDER'] = os.path.join(os.getcwd(), "static", "upscaled")
+app = Flask(__name__, template_folder='templates', static_folder='static')
+cred = credentials.Certificate("firebase-sdk.json")
+firebase_admin.initialize_app(
+    cred, {'storageBucket': 'practice-e99ea.appspot.com'})
+
+bucket = storage.bucket()
 
 
 @app.route("/")
@@ -17,43 +17,26 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/download", methods=["POST"])
-def download():
+@app.route("/upload", methods=["POST"])
+def upload():
     file = request.files['uploadedFile']
-
     if file:
         filename = secure_filename(file.filename)
+        blob = bucket.blob(filename)
+        blob.upload_from_file(file)
+        output_image_url = blob.public_url
 
-        input_path = os.path.join(
-            app.config['UPLOAD_FOLDER'], str(time.time()) + '_' + filename)
-
-        file.save(input_path)
-
-        output_path = os.path.join(app.config['UPSCALED_FOLDER'], str(
-            time.time()) + '_upscaled_' + filename)
-
-        enhance_image(input_path, output_path)
-
-        after_preview_path = url_for(
-            'static', filename='upscaled/' + os.path.basename(output_path))
-
-        return render_template('preview.html', download_img=output_path, output_image=after_preview_path)
+        return render_template('preview.html', download_img=filename, output_image=output_image_url)
     else:
         return redirect(url_for("home"))
 
 
 @app.route("/download/<filename>")
-def download_image(filename):
-    download_file = send_file(filename, as_attachment=True)
-    return download_file
-
-
-def enhance_image(input, output):
-    realesrgan_path = os.path.join(
-        os.getcwd(), "RealESRGAN", "realesrgan-ncnn-vulkan.exe")
-    command_statement = [realesrgan_path, '-i', input, '-o', output]
-    subprocess.Popen(command_statement,
-                     creationflags=subprocess.CREATE_NO_WINDOW).wait()
+def download(filename):
+    blob = bucket.blob(filename)
+    download_url = blob.generate_signed_url(
+        version="v4", method="GET", expiration=3600)
+    return redirect(download_url)
 
 
 if __name__ == "__main__":
